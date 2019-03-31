18498a21dcd048cd3658a765fdecacc89fa108cd
xionghuilin <928089510@qq.com>
2019-03-31 22:07:20 +0800

    spring相关

---
+++ simple-resttemplate-demo/src/main/java/com/xiong/study/spring/simpleresttemplatedemo/SimpleResttemplateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xiong.study.spring.simpleresttemplatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleResttemplateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleResttemplateDemoApplication.class, args);
+	}
+
+}
---
+++ simple-resttemplate-demo/src/main/resources/doc/readme.md
@@ -0,0 +1,1 @@
+#this is simple rest template demo
